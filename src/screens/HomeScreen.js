d1fad002da770bab638fcb2786e2dc9cfa4cfabf
Deepak Rajkumar <deepakdeepakeng@gmail.com>
2023-08-06 11:34:44 +0530

    Add features component

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,10 +1,27 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import {View, Text, SafeAreaView, Image} from 'react-native';
+import React, {useState} from 'react';
+import {
+  widthPercentageToDP as wp,
+  heightPercentageToDP as hp,
+} from 'react-native-responsive-screen';
+import Features from '../components/features';
 
 export default function HomeScreen() {
+  const [messages, setMessages] = useState([]);
   return (
-    <View>
-      <Text>HomeScreen</Text>
+    <View className="flex-1 bg-white">
+      <SafeAreaView className="mx-5">
+        <View className="items-center">
+          <Image
+            source={require('../../assets/images/bot.png')}
+            style={{height: hp(15), width: hp(15)}}
+          />
+        </View>
+
+        {/* messages || features */}
+
+        {messages.length > 0 ? <View> </View> : <Features />}
+      </SafeAreaView>
     </View>
-  )
+  );
 }
